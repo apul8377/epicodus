0a94a3a28a859f445814a05c479c893210418258
Alex Pulvers <apulvers@my.chemeketa.edu>
2020-05-31 14:58:06 -0700

    js practice 5/30/2020

---
+++ practice-5-30-2020.js
@@ -0,0 +1,27 @@
+const getPersonsName = function(PersonsName){
+PersonName = prompt("please enter your name");
+alert("Hello, " + PersonName + "!");
+};
+getPersonsName();
+
+const multiply = function(number1, number2) {
+const result =  100 / 5;
+ alert(result);
+ };
+ multiply();
+
+const divide = function(number1, number2) {
+const result =  100 / 5;
+ alert(result);
+ const remainder = 100 % 6;
+ alert(remainder);
+ };
+ divide();
+
+const userInfo = function(inputtedAge, prompt, favoriteFood){
+	const inputtedAge = prompt("Enter your age: ");
+	const name = prompt("Enter your name: ");
+	const favoriteFood = prompt("Enter your favorite food: ");
+	alert(userInfo);
+};
+userInfo();
