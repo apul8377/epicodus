ce9cdf589e68de3d3fc61ce9a229ccd934a6c650
Alex Pulvers <apulvers@my.chemeketa.edu>
2020-06-01 13:36:20 -0700

    calculator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 # epicodus
 used for school
+=======
+#This program is a simple calculator.
+#enter two numbers and the result will be displayed on your screen.
+>>>>>>> 470d40f... This is the calculator project
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const add = function(number1, number2){
+	return number1 + number2;
+  }
+	number1 = parseInt(prompt("Please enter a number"));
+  number2 = parseInt(prompt("Please enter another number"));
+  const result = add(number1, number2);
+
+alert(result);
