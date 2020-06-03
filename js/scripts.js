fa399c8c1ad4c463bbae6cd2cc1074eecf690b37
Alex Pulvers <apulvers@my.chemeketa.edu>
2020-06-02 20:59:50 -0700

    adding 6.2.20 work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const add = function(number1, number2){
 	return number1 + number2;
   }
@@ -6,3 +7,31 @@ const add = function(number1, number2){
   const result = add(number1, number2);
 
 alert(result);
+=======
+// $(document).ready(function() {
+//     $(".clickable").click(function() {
+//     $("#walrus-showing").toggle();
+//     $("#walrus-hidden").toggle();
+//   });
+// });
+
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $("person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+  });
+});
+>>>>>>> fc33d48... code practice 6/2/2020
